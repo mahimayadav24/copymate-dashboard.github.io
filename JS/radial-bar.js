40cac07d6facfb3fb1ebbd82668df6469295a6e4
mahimayadav24 <88049838+mahimayadav24@users.noreply.github.com>
2021-12-22 12:35:51 +0530

    insert

---
+++ JS/radial-bar.js
@@ -0,0 +1,61 @@
+
+
+var options = {
+  series: [74],
+  chart: {
+    type: 'radialBar',
+    offsetY: -20,
+    sparkline: {
+      enabled: true
+    }
+  },
+  plotOptions: {
+    radialBar: {
+      startAngle: -90,
+      endAngle: 90,
+      track: {
+        background: "#e7e7e7",
+        strokeWidth: '90%',
+        margin: 10, // margin is in pixels
+        dropShadow: {
+          enabled: true,
+          top: 2,
+          left: 0,
+          color: '#999',
+          opacity: 1,
+          blur: 2
+        }
+      },
+      dataLabels: {
+        name: {
+          show: false
+        },
+        value: {
+          offsetY: -2,
+          fontWeight: 600,
+          fontSize: '22px'
+        }
+      }
+    }
+  },
+  grid: {
+    padding: {
+      top: -10
+    }
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'light',
+      shadeIntensity: 0.4,
+      inverseColors: false,
+      opacityFrom: 1,
+      opacityTo: 1,
+      stops: [0, 50, 53, 91]
+    },
+  },
+  labels: ['Average Results'],
+};
+
+var chart = new ApexCharts(document.querySelector("#SemiCircle"), options);
+chart.render();
